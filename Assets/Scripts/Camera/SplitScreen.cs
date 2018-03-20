1ed7688766396aa81751369dfab26c3c24dfa647
Luc-Frédéric Langis <lucf.langis@gmail.com>
2018-03-20 14:10:33 +0100

    feat(scene): add dynamic split screen demo scene

--- Assets/Scripts/Camera/SplitScreen.cs
+++ Assets/Scripts/Camera/SplitScreen.cs
@@ -10,12 +10,19 @@ namespace CameraUtils {
   /// </summary>
   public class SplitScreen : MoreMountains.CorgiEngine.CameraController {
 
-		/// <summary>
-		/// Set new target
-		/// </summary>
-		/// <param name="target"></param>
+    /// <summary>
+    /// Set new target for camera
+    /// </summary>
+    /// <param name="target"></param>
     public void SetTarget(Transform target) {
-			_target = target;
+      _target = target;
+
+      if (_target.GetComponent<CorgiController>() == null) {
+        Debug.LogWarning("CameraController : The Player character doesn't have a CorgiController associated to it, the Camera won't work.");
+        return;
+      }
+
+      _targetController = target.GetComponent<CorgiController>();
     }
   }
 }
